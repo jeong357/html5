8f76f0492c31a353b04b8434220df42aee5c4312
Your Name <you@example.com>
2025-11-12 10:23:03 +0900

    20251112

--- PJ/picogram/picogram_sub1/picogram_sub1.js
+++ PJ/picogram/picogram_sub1/picogram_sub1.js
@@ -7,7 +7,6 @@ document.addEventListener("DOMContentLoaded", () => {
     familyBox.classList.toggle("active");
   });
 
-  // 바깥 클릭 시 닫기
   document.addEventListener("click", () => {
     familyBox.classList.remove("active");
   });
@@ -16,15 +15,8 @@ const slides = document.querySelectorAll(".fade-slider img");
 let current = 0;
 
 function showNext() {
-  // 현재 이미지 제거
   slides[current].classList.remove("active");
-
-  // 다음 인덱스로 이동
   current = (current + 1) % slides.length;
-
-  // 다음 이미지 표시
   slides[current].classList.add("active");
 }
-
-// 3초마다 다음 이미지로 변경
 setInterval(showNext, 3000);
